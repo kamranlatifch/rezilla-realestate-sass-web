cb120d41ccd0d93f03256cb4f48dd5fb3140f0e9
kamranlatifch <kamranlatif98@gmail.com>
2024-11-01 17:50:36 +0500

    Initial setup and banner design

---
+++ src/components/InfoBar.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { ReactComponent as MapPin } from '../assets/svgs/MapPin.svg';
+import { ReactComponent as Phone } from '../assets/svgs/Phone.svg';
+import { ReactComponent as Email } from '../assets/svgs/Email.svg';
+
+const InfoBar = () => {
+  return (
+    <div className='info-bar'>
+      <div className='address'>
+        <MapPin class />
+
+        <div className='small-paragraph'>Rezilla, 18 Grattan St, Brooklyn</div>
+      </div>
+      <div className='info-details'>
+        <div className='contact'>
+          <Phone />
+          <div className='small-paragraph'> +1 206-214-2298</div>
+        </div>
+        <div className='contact'>
+          <Email />
+          <div className='small-paragraph'>support@rezilla.com</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default InfoBar;
