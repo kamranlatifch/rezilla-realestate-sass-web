86bd17e4d72a3b8839b38361fd0a57be940f9807
Muhammad Kamran Latif <111248908+kamranlatifch@users.noreply.github.com>
2024-11-05 14:50:46 +0500

    UI/neighbourhood (#4)
    * add property listing section with styling
    
    * ui/add neighbourhood section (#3)
    
    * UI Neighbourhood add

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import InfoBar from './components/InfoBar';
 import HeroHeader from './components/HeroHeader';
 import TrustedClients from './components/TrustedClients';
 import AboutUs from './components/AboutUs';
+import PropertyListing from './components/PropertyListing';
+import OurServices from './components/OurServices';
+import Neighbourhood from './components/Neighbourhood';
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
       <HeroHeader />
       <TrustedClients />
       <AboutUs />
+      <PropertyListing />
+      <OurServices />
+      <Neighbourhood />
     </>
   );
 }
